97cea3b544e86b06a1004052b9fa28dc16ec9432
Joserbp <josera.jack@gmail.com>
2024-05-27 15:34:56 -0600

    Add class Prueba

---
+++ MLBL/Prueba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLBL
+{
+    public class Prueba
+    {
+        public int IdPrueba { get; set; }
+
+    }
+}
+
+
