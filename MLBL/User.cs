243165dc3e31a22965d2a150306295a915098c95
AlanACP <alancuestapineda@gmail.com>
2024-05-27 17:18:51 -0600

    Add Class User

---
+++ MLBL/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLBL
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+    }
+}
