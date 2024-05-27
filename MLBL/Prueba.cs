7630e1354a441e64f89529ceca65209ba08656c8
AlanACP <alancuestapineda@gmail.com>
2024-05-27 15:53:19 -0600

    Change Telefono

--- MLBL/Prueba.cs
+++ MLBL/Prueba.cs
@@ -13,7 +13,7 @@ namespace MLBL
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public int Edad { get; set; }
-        public string Telefono { get; set; }
+        public string Celular { get; set; }
     }
 }
 
